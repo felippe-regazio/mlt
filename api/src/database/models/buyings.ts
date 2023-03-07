2c570a59f20ae02b9abe4da9db0c684382fa1ff7
Felippe Regazio <felippe.moraes@zoho.com>
2023-03-07 04:41:50 -0300

    intial commit

--- api/src/database/models/buyings.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const BuyingsSchema = new mongoose.Schema({
-	payment: { type:Object },
-  userId: { type: mongoose.Types.ObjectId }
-});
-
-export default mongoose.model('buyings', BuyingsSchema);
--- api/src/endpoints/index.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-module.exports = function index(_: Request, res: Response) {
-  res.status(200).json({
-    success: true,
-    message: 'This is a health check endpoint'
-  });
-}
